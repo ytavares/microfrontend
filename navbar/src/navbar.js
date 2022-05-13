cd789ee4cfcfe95ea61936464816356a434421ca
ytavares <yuri.tavares@weef.com.br>
2022-05-12 22:58:03 -0300

    feat(footer): adiciona footer em angular

--- navbar/src/navbar.js
+++ navbar/src/navbar.js
@@ -65,9 +65,9 @@ export default function BasicTabs() {
     <Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
+          <Tab label="Home" {...a11yProps(0)} />
+          <Tab label="App1" {...a11yProps(1)} />
+          <Tab label="App2" {...a11yProps(2)} />
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
